4ecbe38dbfa79c2e4885a5d219bb95d54725c5df
Sergio <sergiomoreno@opendeusto.es>
2024-01-04 11:09:14 +0100

    api subida

--- API_REST/api.py
+++ API_REST/api.py
@@ -1,1 +1,12 @@
-#
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def hola_mundo():
+    return {
+            "codigo": 200,
+            "message": "Hola Mundo"
+            }
+
+# @app.
