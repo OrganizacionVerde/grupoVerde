759259a079036b1693d425a86f2b82dd12360993
leyreguerra <leyre.guerra@opendeusto.es>
2024-01-07 18:14:04 +0100

    prueba funcional de subida de datos mongo

---
+++ mongo.py
@@ -0,0 +1,12 @@
+from pymongo import MongoClient
+
+# Conectar a la base de datos de MongoDB
+cliente = MongoClient('localhost', 27017)
+db = cliente['Michelin_Storage']
+coleccion = db['stock']
+
+def almacenar_en_mongo(datos):
+    print(datos)
+    coleccion.insert_one(document=datos)
+    print("Datos almacenados")
+    return {"mensaje": "Datos almacenados en MongoDB"}
