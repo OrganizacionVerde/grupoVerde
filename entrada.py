17f2f64ca3d7f8164d4ec5006e0e4645d8c5d38d
leyreguerra <leyre.guerra@opendeusto.es>
2024-01-07 18:17:40 +0100

    prueba funcional de subida de datos mongo

--- entrada.py
+++ entrada.py
@@ -2,10 +2,7 @@ import pandas as pd
 from fastapi import FastAPI
 import schedule
 import time
-<<<<<<< Updated upstream
-=======
 from mongo import almacenar_en_mongo
->>>>>>> Stashed changes
 
 app = FastAPI()
 
@@ -13,31 +10,20 @@ app = FastAPI()
 excel_file = 'Excel.xlsx'
 data = pd.read_excel(excel_file)
 
-<<<<<<< Updated upstream
-=======
 #@app.post("/")
->>>>>>> Stashed changes
 def fila_aleatoria():
     # Seleccionar aleatoriamente una fila del DataFrame
     fila_aleatoria = data.sample()
 
-<<<<<<< Updated upstream
     # Abrir el archivo en modo de adición y añadir la fila como un nuevo registro
     with open('entrada.jsonl', 'a') as json_file:
         fila_aleatoria.to_json(json_file, orient='records', lines=True)
-    return fila_aleatoria
-=======
-    fila_dict = fila_aleatoria.to_dict(orient='records')[0]
 
+    fila_dict = fila_aleatoria.to_dict(orient='records')[0]
     print(fila_dict)
-    # Abrir el archivo en modo de adición y añadir la fila como un nuevo registro
-    with open('entrada.jsonl', 'a') as json_file:
-        json = fila_aleatoria.to_json(json_file, orient='records', lines=True)
-    #print(json)
     almacenar_en_mongo(fila_dict)
-    #return json
     return fila_dict
->>>>>>> Stashed changes
+
 
 # Programar la ejecución de obtener_datos cada 3 segundos
 schedule.every(3).seconds.do(fila_aleatoria)
