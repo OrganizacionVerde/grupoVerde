09732c1e8e572d761d9bc0e23ea73fde335d7657
Sergio <sergiomoreno@opendeusto.es>
2024-01-07 13:48:56 +0100

    creado el modelo Lote, que hereda de Referencia

--- API_REST/main.py
+++ API_REST/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from modelos import Referencia
+from modelos import Referencia, Lote
 
 app = FastAPI()
 
--- API_REST/modelos.py
+++ API_REST/modelos.py
@@ -1,7 +1,21 @@
 from pydantic import BaseModel
-
+from datetime import datetime
 
 class Referencia(BaseModel):
+    Nombre: str
+    Tipo: str
+    Estacion: str
+    Eficiencia_energetica: str
+    Capacidad_frenado: str
+    Ruido_ambiental_dB: int
+    Ruido_ambiental_ondas: int
+    Dimension: str
+
+class Lote(Referencia):
+    ID_lote: int
+    Cantidad_neumaticos: int
+    fecha_entrada: datetime
+    fecha_salida: datetime = None
     Nombre: str
     Tipo: str
     Estacion: str
