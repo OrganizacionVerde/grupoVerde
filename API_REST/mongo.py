ec1e3369669e77177de36099f25f259a21e596d0
sergiomorenohernandez <79530953+sergiomorenohernandez@users.noreply.github.com>
2024-01-15 11:53:40 +0100

    Modificación mongodb.py
    Cambio de mongodb.py para que conecte a MongoDB Atlas (cloud) en vez de a MongoDB Compass (localhost)

--- API_REST/mongo.py
+++ API_REST/mongo.py
@@ -1,11 +1,18 @@
 from pymongo import MongoClient
 
-# Conectar a la base de datos de MongoDB
-cliente = MongoClient('localhost', 27017)
+# Usuario y contraseña del cluster0 en MongoDB Atlas
+user = 'dbUser'
+user_password = 'dbUserPassword'
+
+# Cadena de conexión
+uri = f"mongodb+srv://{user}:{user_password}@cluster0.xiuhogh.mongodb.net/?retryWrites=true&w=majority"
+
+# Conexión a la base de datos de MongoDB Atlas
+cliente = MongoClient(uri)
 db = cliente['Michelin_Storage']
-coleccion = db['stock']
 
 def almacenar_en_mongo(datos):
+    coleccion = db['stock']
     print(datos)
     coleccion.insert_one(document=datos)
     return {"mensaje": "Datos almacenados en MongoDB"}
